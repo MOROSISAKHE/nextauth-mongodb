55dea60d72096adbb2b1ff0e90e89580788386f5
SAKHE MOROSI <netcampus.morosi@mercedes-benz.com>
2024-08-23 15:41:27 +0200

    added nextui library

--- app/Providers.js
+++ app/Providers.js
@@ -1,6 +1,12 @@
 "use client";
+import react from "react";
 import { SessionProvider } from "next-auth/react";
+import { NextUIProvider } from "@nextui-org/react";
 
 export const AuthProvider = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
 };
