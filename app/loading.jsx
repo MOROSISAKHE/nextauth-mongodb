130c854790872535fbe5dc01bb3d4a10b5fc0285
SAKHE MOROSI <netcampus.morosi@mercedes-benz.com>
2024-08-23 15:42:24 +0200

    modified

--- app/loading.jsx
+++ app/loading.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import {Spinner} from "@nextui-org/spinner";
 const SpinningLoadingPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <p className="text-black font bold">Loading...</p>
       <Spinner/>
     </div>
   );
